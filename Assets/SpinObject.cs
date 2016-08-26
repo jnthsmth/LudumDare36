8950c2811b863d33f2667b9ef2e7d0a30f62bc31
Jonathan Smith <jnthsmth@umich.edu>
2016-08-25 17:45:07 -0700

    Initial Commit
    Added wacky spinning stuff in a scene, to prove to people that their
    tools work when cloning the project for the first time.

---
+++ Assets/SpinObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SpinObject : MonoBehaviour {
+
+	public float speed = 4f;
+
+	void Start () {
+		GetComponent<Rigidbody>().angularDrag = 0.0f;
+		GetComponent<Rigidbody>().angularVelocity = Random.onUnitSphere * speed;
+	}
+}

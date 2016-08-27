879d3e2197303dba9c295d568e10b0b9f2a11090
Jonathan Smith <jnthsmth@umich.edu>
2016-08-26 20:56:09 -0700

    Tuning movement

--- Assets/PlatformerControls.cs
+++ Assets/PlatformerControls.cs
@@ -6,14 +6,11 @@ public class PlatformerControls : MonoBehaviour {
 
 	public Vector2 controlVector = Vector2.zero;
 	public bool grounded = true;
-	public float jumpResetTime = 0f;
-	public float jumpTimer = 0f;
-	public float horizontalControlSnap = 10f;
 	public Vector2 responsiveness = new Vector2 (10f, 0f);
 	public Vector2 speedLimits = new Vector2(10f, 10f);
-	public float speedScale = 4f;
+	public float speedScale = 10f;
 	private Rigidbody rig;
-	public Vector3 jumpVector = new Vector3(0f,7f,0f);
+	public Vector3 jumpVector = new Vector3(0f,8f,0f);
 	private bool jump = false;
 
 	void Awake() {
@@ -21,11 +18,8 @@ public class PlatformerControls : MonoBehaviour {
 	}
 
 	void Update () {
-		jumpTimer -= Time.deltaTime;
-		if (jumpTimer <= 0f) jumpTimer = 0f;
 		controlVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-		if (Input.GetKeyDown (KeyCode.Space) && grounded && jumpTimer == 0f) {
-			jumpTimer = jumpResetTime;
+		if (Input.GetKeyDown (KeyCode.Space) && grounded) {
 			jump = true;
 		}
 	}
@@ -44,7 +38,7 @@ public class PlatformerControls : MonoBehaviour {
 	}
 
 	Vector3 applyControls(Vector3 vec) {
-		vec.x = Mathf.MoveTowards (vec.x, controlVector.x * speedScale, responsiveness.x);
+		vec.x = controlVector.x * speedScale;
 		if (jump) {
 			jump = false;
 			vec += jumpVector;

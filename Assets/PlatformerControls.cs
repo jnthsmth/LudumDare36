ccdfd610c50d097a381b9b25070afe1d0ebc4176
Jonathan Smith <jnthsmth@umich.edu>
2016-08-26 20:09:13 -0700

    Control first draft
    Added an example character in a scene for our 2d platformer controls.

---
+++ Assets/PlatformerControls.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlatformerControls : MonoBehaviour {
+
+	public Vector2 controlVector = Vector2.zero;
+	public bool grounded = false;
+	public float jumpResetTime = 1f;
+	public float jumpTimer = 0f;
+	public float horizontalControlSnap = 10f;
+	public Vector2 responsiveness = new Vector2 (10f, 0f);
+	public float speedScale = 4f;
+	private Rigidbody rig;
+	public Vector3 jumpVector = new Vector3(0f,7f,0f);
+
+	void Awake() {
+		rig = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		jumpTimer -= Time.deltaTime;
+		if (jumpTimer <= 0f) jumpTimer = 0f;
+		controlVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		if (Input.GetKeyDown (KeyCode.Space) && grounded && jumpTimer == 0f) {
+			jumpTimer = jumpResetTime;
+			rig.AddForce(jumpVector, ForceMode.Impulse);
+		}
+	}
+
+	void FixedUpdate() {
+		Vector3 vel = rig.velocity;
+		vel.x = Mathf.MoveTowards (vel.x, controlVector.x * speedScale, responsiveness.x);
+		rig.velocity = vel;
+
+	}
+}

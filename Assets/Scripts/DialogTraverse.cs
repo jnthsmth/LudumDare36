4652ecc58cf04b89909c6249ee0b6ea75244863d
Jonathan Smith <jnthsmth@umich.edu>
2016-08-27 12:56:56 -0700

    dialog options going into a new refactor

--- Assets/Scripts/DialogTraverse.cs
+++ Assets/Scripts/DialogTraverse.cs
@@ -2,21 +2,27 @@
 using System.Collections;
 using System.Collections.Generic;
 
+[System.Serializable]
+public class NPCDialog {
+	public string optionDescription = "Talk";
+	public string message = "Hello World!";
+	public NPCDialog[] subOptions = null;
+}
 
 public class DialogTraverse : MonoBehaviour {
 
-	public NPCDialog[] root;
+	public NPCDialog root;
 	private List<int> history = new List<int>();
 	private int _selectedIdx = 0;
 	private int selectedIdx {
 		get { return _selectedIdx; }
-		set { selectedIdx = value % root.Length;
+		set { selectedIdx = value % root.subOptions.Length;
 			setDisplayText(); }
 	}
 
 	private NPCDialog selectedNode {
 		get { 
-			NPCDialog node = root[0];
+			NPCDialog node = root;
 			foreach (int i in history) node = node.subOptions[i];
 			return node;
 		}
@@ -24,9 +30,9 @@ public class DialogTraverse : MonoBehaviour {
 
 	void setDisplayText () {
 		string displayText = selectedNode.message;
-		for (int i = 0; i < selectedNode.subOptions.Length; i++) 
+		for (int i = 0; i < selectedNode.subOptions.Length; i++) {
 			displayText += "\n" + ((i == selectedIdx) ? ">" : "") + selectedNode.subOptions [i].optionDescription;
-		
+		}
 		GetComponent<TextMesh> ().text = displayText;
 	}
 
--- Assets/Scripts/NPCDialog.cs
+++ Assets/Scripts/NPCDialog.cs
@@ -1,11 +1,3 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[System.Serializable]
-public class NPCDialog {
-
-	public string optionDescription = "Talk";
-	public string message = "Hello World!";
-	public NPCDialog[] subOptions;
-
-}

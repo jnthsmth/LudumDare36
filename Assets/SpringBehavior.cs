65189024fd09e25a2c2ce7f0ce63ac7aa4d7df61
DimensionalCode <noel.bhattacharyya@gmail.com>
2016-08-27 02:20:40 -0400

    Spring Stuff

---
+++ Assets/SpringBehavior.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SpringBehavior : MonoBehaviour {
+
+	private Rigidbody rig;
+	public float force = 2f;
+
+	void Awake() {
+		rig = GetComponent<Rigidbody> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Rigidbody otherRig = other.gameObject.GetComponent<Rigidbody> ();
+		otherRig.velocity += transform.up * force;
+	}
+}

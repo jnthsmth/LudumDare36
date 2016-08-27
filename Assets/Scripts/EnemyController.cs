e6b5cd497c6ddb8c4f645310526709de3f3e37fd
Jonathan Smith <jnthsmth@umich.edu>
2016-08-27 15:09:13 -0700

    some basic enemy env detection with recasts

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	public bool groundAhead = false;
+	public bool pathObstructed = false;
+
+	void FixedUpdate () {
+		Vector3 atGroundAhead = transform.TransformDirection (new Vector3 (1f, -0.6f, 0f));
+		groundAhead = Physics.Raycast (transform.position, atGroundAhead, atGroundAhead.magnitude);
+		RaycastHit h = new RaycastHit();
+		pathObstructed = false;
+		if ( Physics.Raycast(new Ray(transform.position,Vector3.right), out h, 1.3f) &&
+				h.collider.gameObject.tag == "Obstruction")
+			pathObstructed = true;
+		
+	}
+}

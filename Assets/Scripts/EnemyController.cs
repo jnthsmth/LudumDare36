d09e25212de7f62d2ea930ce59754496f00df395
Jonathan Smith <jnthsmth@umich.edu>
2016-08-27 15:20:49 -0700

    enemy has working sentry mode, and trigger regions for claws and sneak kill

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,8 +5,9 @@ public class EnemyController : MonoBehaviour {
 
 	public bool groundAhead = false;
 	public bool pathObstructed = false;
+	public float speed = 1f;
 
-	void FixedUpdate () {
+	void Update () {
 		Vector3 atGroundAhead = transform.TransformDirection (new Vector3 (1f, -0.6f, 0f));
 		groundAhead = Physics.Raycast (transform.position, atGroundAhead, atGroundAhead.magnitude);
 		RaycastHit h = new RaycastHit();
@@ -14,6 +15,13 @@ public class EnemyController : MonoBehaviour {
 		if ( Physics.Raycast(new Ray(transform.position,Vector3.right), out h, 1.3f) &&
 				h.collider.gameObject.tag == "Obstruction")
 			pathObstructed = true;
-		
+
+		if (!groundAhead || pathObstructed) {
+			Vector3 angles = transform.rotation.eulerAngles;
+			angles.y += 180;
+			transform.rotation = Quaternion.Euler (angles);
+		} else {
+			transform.position += transform.right * Time.deltaTime *speed;
+		}
 	}
 }

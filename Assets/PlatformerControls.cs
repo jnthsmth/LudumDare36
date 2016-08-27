92ba214b360b7d9d6561031437f1384e381c8e38
Jonathan Smith <jnthsmth@umich.edu>
2016-08-26 20:34:09 -0700

    Add speed limits
    10f 10f currently

--- Assets/PlatformerControls.cs
+++ Assets/PlatformerControls.cs
@@ -10,6 +10,7 @@ public class PlatformerControls : MonoBehaviour {
 	public float jumpTimer = 0f;
 	public float horizontalControlSnap = 10f;
 	public Vector2 responsiveness = new Vector2 (10f, 0f);
+	public Vector2 speedLimits = new Vector2(10f, 10f);
 	public float speedScale = 4f;
 	private Rigidbody rig;
 	public Vector3 jumpVector = new Vector3(0f,7f,0f);
@@ -19,7 +20,6 @@ public class PlatformerControls : MonoBehaviour {
 		rig = GetComponent<Rigidbody> ();
 	}
 
-	// Update is called once per frame
 	void Update () {
 		jumpTimer -= Time.deltaTime;
 		if (jumpTimer <= 0f) jumpTimer = 0f;
@@ -32,11 +32,23 @@ public class PlatformerControls : MonoBehaviour {
 
 	void FixedUpdate() {
 		Vector3 vel = rig.velocity;
-		vel.x = Mathf.MoveTowards (vel.x, controlVector.x * speedScale, responsiveness.x);
+		vel = applyControls(vel);
+		vel = applySpeedLimits(vel);
+		rig.velocity = vel;
+	}
+
+	Vector3 applyControls(Vector3 vec) {
+		vec.x = Mathf.MoveTowards (vec.x, controlVector.x * speedScale, responsiveness.x);
 		if (jump) {
 			jump = false;
-			vel += jumpVector;
+			vec += jumpVector;
 		}
-		rig.velocity = vel;
+		return vec;
+	}
+
+	Vector3 applySpeedLimits(Vector3 vec) {
+		vec.x = Mathf.Min (vec.x, speedLimits.x);
+		vec.y = Mathf.Min (vec.y, speedLimits.y);
+		return vec;
 	}
 }

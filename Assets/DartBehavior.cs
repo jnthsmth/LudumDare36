66521439a3931e91db37e8c7f3260ef65ad676b2
DimensionalCode <noel.bhattacharyya@gmail.com>
2016-08-27 19:58:01 -0400

    Save!

---
+++ Assets/DartBehavior.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class DartBehavior : MonoBehaviour {
+
+	private Rigidbody rig;
+
+	void Awake() {
+		rig = GetComponent<Rigidbody> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Euler(rig.velocity);
+		transform.FindChild("Sprite").transform.rotation = transform.rotation;
+	}
+
+	void UpdateFixed() {
+		transform.rotation = Quaternion.Euler(rig.velocity); Quaternion.
+		transform.FindChild("Sprite").transform.rotation = transform.rotation;
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		Destroy(this.gameObject);
+	}
+
+
+}

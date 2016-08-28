18839b5fdbfa6006a626a16d0539069b65e26227
DimensionalCode <noel.bhattacharyya@gmail.com>
2016-08-28 13:56:43 -0400

    Minor Updates

--- Assets/Scripts/DartBehavior.cs
+++ Assets/Scripts/DartBehavior.cs
@@ -7,8 +7,10 @@ public class DartBehavior : MonoBehaviour {
 	private Rigidbody rig;
 	public float damage = 10f;
 	public bool active = false;
+	private bool wasActive = false;
 	public float despawnTime = 10f;
-	private Vector3 velocity;
+	private bool despawn = false;
+	private Vector3 velocity = Vector3.left;
 
 	void Awake() {
 		rig = GetComponent<Rigidbody> ();
@@ -16,16 +18,18 @@ public class DartBehavior : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if(active) {
 			transform.rotation = Quaternion.AngleAxis(Vector3.Angle(Vector3.left, velocity) * (velocity.y > 0?-1f:1f), Vector3.forward);
-			if(despawnTime <= 0) {
-				Destroy(this.gameObject);
-			}
+		}else if(rig.velocity.magnitude > 3f && wasActive) {
+			transform.rotation = Quaternion.AngleAxis(Vector3.Angle(Vector3.left, rig.velocity) * (rig.velocity.y > 0?-1f:1f), Vector3.forward);
+			Activate(rig.velocity);
+		}
+		if(despawnTime <= 0) {
+			Destroy(this.gameObject);
 		}
 	}
 
@@ -33,6 +37,8 @@ public class DartBehavior : MonoBehaviour {
 		if(active) {
 			velocity += Physics.gravity * Time.deltaTime;
 			rig.position += velocity * Time.deltaTime;
+		}
+		if(despawn) {
 			despawnTime -= Time.deltaTime;
 		}
 	}
@@ -42,18 +48,24 @@ public class DartBehavior : MonoBehaviour {
 			if(other.isTrigger) return;
 			Character target = other.GetComponentInParent<Character>();
 			if(target != null) target.health -= damage;
-			Destroy(this.gameObject);
+			rig.isKinematic = false;
+			rig.useGravity = true;
+			velocity = other.attachedRigidbody.velocity;
+			Deactivate();
 		}
 	}
 
 	public void Activate(Vector3 vel) {
-		rig.useGravity = true;
+		rig.isKinematic = true;
+		rig.useGravity = false;
 		velocity = vel;
 		active = true;
+		despawn = true;
 	}
 
 	public void Deactivate() {
+		if(active = true) wasActive = true;
 		active = false;
-		rig.useGravity = false;
+		rig.velocity = velocity;
 	}
 }

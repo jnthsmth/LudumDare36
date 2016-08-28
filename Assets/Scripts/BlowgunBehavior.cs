cc57037e96d8b42f03430c8c5e4bbc92b2b8f017
DimensionalCode <noel.bhattacharyya@gmail.com>
2016-08-28 11:28:26 -0400

    Firing Blowgun Prefab

--- Assets/Scripts/BlowgunBehavior.cs
+++ Assets/Scripts/BlowgunBehavior.cs
@@ -3,6 +3,16 @@ using System.Collections;
 
 public class BlowgunBehavior : MonoBehaviour {
 
+	public float coolDown = 1f;
+	public float timeLeft = 0f;
+	private Rigidbody rig;
+	public bool manualFire = false;
+	public float firingSpeed = 20f;
+
+	void Awake() {
+		rig = GetComponent<Rigidbody> ();
+	}
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +20,30 @@ public class BlowgunBehavior : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(manualFire && timeLeft <= 0f) {
+			manualFire = false;
+			Fire();
+		}
 	}
+
+	void FixedUpdate() {
+		if(timeLeft > 0f) timeLeft -= Time.deltaTime;
+		else if(timeLeft < 0f) timeLeft = 0f;
+	}
+
+	void Fire() {
+		if(timeLeft > 0f) return;
+		timeLeft = coolDown;
+		Vector3 pos = rig.position;
+
+		Transform dart = (Transform) Instantiate(transform.FindChild("Dart"), transform.FindChild("Spawn Location").position, transform.rotation);
+		Rigidbody dartRig = dart.GetComponent<Rigidbody>();
+		DartBehavior dartControl = dart.GetComponent<DartBehavior>();
+
+		Vector3 vel = (dartRig.position - transform.position);
+		vel.Normalize();
+		vel *= firingSpeed;
+		dartControl.Activate(vel);
+	}
+
 }
--- Assets/Scripts/DartBehavior.cs
+++ Assets/Scripts/DartBehavior.cs
@@ -6,6 +6,9 @@ public class DartBehavior : MonoBehaviour {
 
 	private Rigidbody rig;
 	public float damage = 10f;
+	public bool active = false;
+	public float despawnTime = 10f;
+	private Vector3 velocity;
 
 	void Awake() {
 		rig = GetComponent<Rigidbody> ();
@@ -18,14 +21,38 @@ public class DartBehavior : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.rotation = Quaternion.AngleAxis(Vector3.Angle(Vector3.left, rig.velocity), Vector3.forward);
+		if(active) {
+			transform.rotation = Quaternion.AngleAxis(Vector3.Angle(Vector3.left, velocity) * (velocity.y > 0?-1f:1f), Vector3.forward);
+			if(despawnTime <= 0) {
+				Destroy(this.gameObject);
+			}
+		}
+	}
+
+	void FixedUpdate() {
+		if(active) {
+			velocity += Physics.gravity * Time.deltaTime;
+			rig.position += velocity * Time.deltaTime;
+			despawnTime -= Time.deltaTime;
+		}
 	}
 
 	void OnTriggerEnter(Collider other) {
-		Character target = other.GetComponentInParent<Character>();
-		if(target != null) target.health -= damage;
-		Destroy(this.gameObject);
+		if(active) {
+			Character target = other.GetComponentInParent<Character>();
+			if(target != null) target.health -= damage;
+			Destroy(this.gameObject);
+		}
 	}
 
+	public void Activate(Vector3 vel) {
+		rig.useGravity = true;
+		velocity = vel;
+		active = true;
+	}
 
+	public void Deactivate() {
+		active = false;
+		rig.useGravity = false;
+	}
 }

dc555932247affae5703bfea2cc6e8b7115aa1d5
DimensionalCode <noel.bhattacharyya@gmail.com>
2016-08-28 16:30:17 -0400

    Cam Controls

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,13 +3,6 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-	private Rigidbody rig;
-	private Vector3 velocity = Vector3.zero;
-
-	void Awake() {
-		rig = GetComponent<Rigidbody> ();
-	}
-
 	// Use this for initialization
 	void Start () {
 	
@@ -17,12 +10,10 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-
-	void FixedUpdate() {
 		Vector3 pos = GameManager.instance.player.transform.position;
 		pos.z = transform.position.z;
+		transform.position = pos;
+
 		transform.position = pos;
 
 	}

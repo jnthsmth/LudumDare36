8d72a55868076115662d9e7c1d91e0a3c5d720ab
Jonathan Smith <jnthsmth@umich.edu>
2016-08-28 12:20:23 -0700

    add intractable base class

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ObjectsInRegion))]
+public class Interactable : MonoBehaviour {
+
+	public ObjectsInRegion oir;
+
+	void Awake() {
+		oir = GetComponent<ObjectsInRegion> ();
+	}
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.E) && oir.value.Contains (GameManager.instance.player)) Interact();
+	}
+
+	public virtual void Interact() {}
+}
